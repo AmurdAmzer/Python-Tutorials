f8fa9308c01bbf993be768117a987183b4987c52
AmurdAmzer <amurdamzer@gmail.com>
2024-06-27 01:53:01 -0400

    adding changes to github

---
+++ Functions/funtions_with_parameters.py
@@ -0,0 +1,18 @@
+# With 1 parameter
+def greet(name):
+    print(f"Hello {name}")
+
+greet("Ahmed")
+
+
+# With 2 or more (multiple) parameters 
+
+def greet(first_name, last_name):
+    print(f"Hello {first_name} {last_name}")
+
+greet("Ahmed", "Hamza")
+
+
+
+
+
